79696ceaf851ef20945d0bf1f77fae2806033643
Eloumbat Assoua Albert <assouaalbert@gmail.com>
2020-06-16 04:26:03 +0300

    removing redux logger from for production build

--- clothing-shop/src/redux/store.js
+++ clothing-shop/src/redux/store.js
@@ -4,7 +4,10 @@ import {persistStore} from 'redux-persist';
 //Createstore  is used to create store
 //Middlewares are used to listen for information
 import logger from 'redux-logger'; //Logger contains methods for the middleware
-const middleWares = [logger]; //Loggers tell the state of redux after every action is fired
+const middleWares = []; //Loggers tell the state of redux after every action is fired
+if(process.env.NODE_ENV == 'developement') {
+    middleWares.push(logger);
+}
 export const store = createStore(rootReducer,applyMiddleware(...middleWares)); //There is only one reducer in the create store
 export const persistor = persistStore(store);
 export default {store,persistor};
